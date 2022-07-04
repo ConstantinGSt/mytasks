4b648e0b8c5eadf98454c0773252838e1a7d6df2
ConstantinGSt <stepanyuk.kon@yandex.ru>
2022-07-04 15:10:14 +0700

    add method input book

--- src/tests/books/Bookshelf.java
+++ src/tests/books/Bookshelf.java
@@ -1,9 +1,11 @@
 package tests.books;
-
+import java.util.Scanner;
 public class Bookshelf {
 	
 	Book [] bookShelf = new Book[10];
+	Book book;
     public void MyBookShelf(int inpitMenuOption) {
+    	Scanner scan = new Scanner(System.in);
     	if(inpitMenuOption == 1) { // info for bookShelf
     		int booksInShelf = 0;
 			int freeInShelf = 0;
@@ -19,7 +21,18 @@ public class Bookshelf {
     		System.out.println("\n" + "Книг на полке: " + booksInShelf + "\n" 
     				+ "Свободное место для " + freeInShelf + " книг"  );
     	} else if(inpitMenuOption == 2) {
-
+    		for(int i = 0; i < bookShelf.length; i++) {
+    			if(bookShelf[i] == null) {
+    				System.out.println("Добавьте книгу в следующем формате: Автор Enter" + "\n"
+    			+ "Название Enter"  + "\n" + "год издания Enter");
+    				book = new Book(book.autor, book.title, book.published) {
+    					book.autor = scan.nextLine();
+    					book.title = scan.nextLine();
+    					book.published = scan.nextLine();
+    				}
+    			}
+    		}
+    		
     	} else if(inpitMenuOption == 3) {
 
     	} else if(inpitMenuOption == 4) {
@@ -30,5 +43,4 @@ public class Bookshelf {
 
     	}
     }
-
 }
--- src/tests/books/BookshelfTest.java
+++ src/tests/books/BookshelfTest.java
@@ -3,26 +3,25 @@ package tests.books;
 import java.util.Scanner;
 
 public class BookshelfTest {
-    public static void main(String[] args) {
-        int inputMenuOption;
-        Bookshelf shelf = new Bookshelf();
-        
-        Scanner scan = new Scanner(System.in);
-        
-        System.out.println("     Меню");
-        System.out.println("1. Информация о полке.");
-        System.out.println("2. Добавить книгу.");
-        System.out.println("3. Удалить книгу.");
-        System.out.println("4. Информацяи о книге.");
-        System.out.println("5. ");
-        System.out.println("6. ");
-        System.out.println("Для выбора необходимого действия введите соответсвующий номер,"
-        		+ "для прекращения работы напишите 'end'");
+	public static void main(String[] args) {
+		int inputMenuOption;
+		Bookshelf shelf = new Bookshelf();
 
-        inputMenuOption = scan.nextInt();
-        System.out.println(inputMenuOption);
-        shelf.MyBookShelf(inputMenuOption);
-        
-    }
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("     Меню");
+		System.out.println("1. Информация о полке.");
+		System.out.println("2. Добавить книгу.");
+		System.out.println("3. Удалить книгу.");
+		System.out.println("4. Информацяи о книге.");
+		System.out.println("5. ");
+		System.out.println("6. ");
+		System.out.println("Для выбора необходимого действия введите соответсвующий номер,"
+				+ "для прекращения работы напишите 'end'");
+		inputMenuOption = scan.nextInt();
+		System.out.println(inputMenuOption);
+		shelf.MyBookShelf(inputMenuOption);
+
+	}
 
 }
