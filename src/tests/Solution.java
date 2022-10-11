380852cdcb9535c0ae1581712304aa354a694975
ConstantinGSt <stepanyuk.kon@yandex.ru>
2022-10-11 14:59:48 +0700

    yhhy

--- src/tests/Solution.java
+++ src/tests/Solution.java
@@ -1,38 +1,68 @@
 package tests;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Solution {
+	
+	public static ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(56, 45, 89, 1, 45, 13, 4, 69, 11, -89));
 
-	 public static ArrayList<Integer> numbers = new ArrayList<Integer>();
+    public static void main(String[] args) {
+        print();
+        sort();
+        System.out.println("\nОтсортированные числа в натуральном порядке:");
+        print();
+    }
 
-	    public static void main(String[] args) {
-	        init();
-	        print();
+    public static void sort() {
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            for (int j = 0; j < numbers.size() - i - 1; j++) {
+                if (numbers.get(j) > numbers.get(j + 1)) {
+                    int temp = numbers.get(j);
+                    numbers.set(j, numbers.get(j + 1));
+                    numbers.set(j + 1, temp);
+                }
+            }
+        }
+    }
 
-	        reverse();
-	        print();
-	    }
+    public static void print() {
+        for (int number : numbers) {
+            System.out.print(number + "\t");
+        }
+    }
+}
 
-	    public static void init() {
-	        for (int i = 0; i < 10; i++) {
-	            numbers.add(i, i);
-	        }
-	    }
 
-	    public static void reverse() {
-	        int n = numbers.size() - 1;
-	        for (int i = 0; i < numbers.size(); i++) {
-	            int temp = numbers.get(i);
-	            numbers.add(i, numbers.get(n - i));
-	            numbers.set(n - i, temp);
-	        }
-	    }
-
-	    private static void print() {
-	        for (int number : numbers) {
-	            System.out.println(number);
-	        }
-	    }
-	}
+//	 public static ArrayList<Integer> numbers = new ArrayList<Integer>();
+//
+//	    public static void main(String[] args) {
+//	        init();
+//	        print();
+//
+//	        reverse();
+//	        print();
+//	    }
+//
+//	    public static void init() {
+//	        for (int i = 0; i < 10; i++) {
+//	            numbers.add(i, i);
+//	        }
+//	    }
+//
+//	    public static void reverse() {
+//	        int n = numbers.size() - 1;
+//	        for (int i = 0; i < numbers.size() / 2; i++) {
+//	            int temp = numbers.get(i);
+//	            numbers.set(i, numbers.get(n - i));
+//	            numbers.set(n - i, temp);
+//	        }
+//	    }
+//
+//	    private static void print() {
+//	        for (int number : numbers) {
+//	            System.out.println(number);
+//	        }
+//	    }
+//	}
 
--- src/tests/test.java
+++ src/tests/test.java
@@ -1,36 +1,63 @@
 package tests;
 
 public class test {
-
-	public static int[] numbers = new int[10];
-
+	
+	public static int[] numbers = new int[] {56, 45, 89, 1, 45, 13, 4, 69, 11, -89};
+	
 	public static void main(String[] args) {
-		init();
-		print();
+        print();
+        sort();
+        System.out.println("\nОтсортированные числа в натуральном порядке:");
+        print();
+    }
 
-//		reverse();
+    public static void sort() {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void print() {
+        for (int number : numbers) {
+            System.out.print(number + "\t");
+        }
+    }
+
+//	public static int[] numbers = new int[10];
+//
+//	public static void main(String[] args) {
+//		init();
 //		print();
-	}
-
-	public static void init() {
-		for (int i = 0; i < 10; i++) {
-			numbers[i] = i;
-		}
-	}
-
-	public static void reverse() {
-		int n = numbers.length - 1;
-		for (int i = 0; i < numbers.length / 2; i++) {
-			int temp = numbers[i];
-			numbers[i] = numbers[n - i];
-			numbers[n - i] = temp;
-		}
-	}
-
-	private static void print() {
-		for (int number : numbers) {
-			System.out.println(number);
-		}
-	}
+//
+//	reverse();
+//	print();
+//	}
+//
+//	public static void init() {
+//		for (int i = 0; i < 10; i++) {
+//			numbers[i] = i;
+//		}
+//	}
+//
+//	public static void reverse() {
+//		int n = numbers.length - 1;
+//		for (int i = 0; i < numbers.length / 2; i++) {
+//			int temp = numbers[i];
+//			numbers[i] = numbers[n - i];
+//			numbers[n - i] = temp;
+//		}
+//	}
+//
+//	private static void print() {
+//		for (int number : numbers) {
+//			System.out.println(number);
+//		}
+//	}
 
 }
