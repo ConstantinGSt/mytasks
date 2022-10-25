e9e5d0d080741df39fa15e9d43f7ec9406243ce6
Constantin St <dinscalin.ks@gmail.com>
2022-10-26 01:14:38 +0700

    add solution

--- src/tests/Solution.java
+++ src/tests/Solution.java
@@ -1,35 +1,23 @@
 package tests;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 /* 
-Перепутанные байты
+Пишем символы в файл
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-    	String readPath = "Cat.txt";
-    	File file = new File("Cat2.txt");
-        file.createNewFile();
-    	String writePath = "Cat2.txt";
-    	try (Scanner scan = new Scanner(System.in); FileInputStream inStream = new FileInputStream(readPath); 
-    			FileOutputStream outStream = new FileOutputStream(writePath)) {
-    		//StringBuilder str;
-    		byte[] byteIn = inStream.readAllBytes();
-    		int[] byteOut = new int[byteIn.length];
-    		StringBuilder str = null;
-    		for(int i = 0; i < byteIn.length; i++) {
-    			byteOut[i] = (int) byteIn[i];
-    		}
-    		outStream.write(byteOut).getBytes(StandardCharsets.UTF_8);
-    	} catch(IOException e) {
-    		System.out.println("все упало");
-    	}
-    }
+	public static void main(String[] args) {
+		char[] chars = args[0].toCharArray();
+		try (InputStream stream = System.in; Scanner scanner = new Scanner(stream)) {
+			Files.write(Path.of(scanner.nextLine()), chars);
+		} catch (IOException e) {
+			System.out.println("Something went wrong : " + e);
+		}
+	}
 }
-
