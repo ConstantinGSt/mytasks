719c989a05c19344322b7310590f2120374e3ea7
Constantin St <dinscalin.ks@gmail.com>
2022-12-01 20:12:59 +0700

    myTask go 01.12

--- src/tests/FileAnalyzer.java
+++ src/tests/FileAnalyzer.java
@@ -6,11 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-
-/* 
-Пишем символы в файл
-*/
-
 public class FileAnalyzer {
 
 	private int maxFilenameLength;
