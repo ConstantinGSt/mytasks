2caa7d783c5990b41da2109bb93a946d412dba27
ConstantinGSt <stepanyuk.kon@yandex.ru>
2023-01-13 16:55:54 +0700

    Streams starts

---
+++ src/javarushTests/streams/Owner.java
@@ -0,0 +1,19 @@
+package javarushTests.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Owner {
+	private String name;
+	
+	private List<Animal> pets = new ArrayList<>();
+
+	public Owner(String name) {
+		this.name = name;
+	}
+
+	public List<Animal> getPets() {
+		return pets;
+	}
+
+}
